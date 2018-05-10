3a47a04168433a6a265679e1578c5279802fcbd0
Chris <32319369+cgiven25@users.noreply.github.com>
2018-05-10 13:41:25 -0500

    server.py changes

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template
+from getWeather import getData
+
+rainData, windSpeed, windDirection, tempMaxData, tempMinData, descriptionData, imageData = getData()
+print(rainData)
+
 app = Flask(__name__)
 
-@app.route("/")
+def getTemp():
+    temp = 0
+    temp = (9/5)*(temp)+32
+    temp = round(temp, 1)
+    return (temp)
+@app.route("/")    
 def index():
-    print "Hi"
-    return "sup"
-
-def getData():
-    return "30%", "65F", "HIGH"
-
-@app.route("/data")
-def hello():
-    precip, temp, pollen = getData()
-    return render_template("page.html", precip = precip, temp = temp, pollen = pollen)
+    temp = getTemp()
+    templateData = {'temperature' : (temp) }
+    return render_template("indexr2.html", temperature=temp )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0")
+    
