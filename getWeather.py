7373144fd275f2761e35d935024a248d803ce4b1
Chris <32319369+cgiven25@users.noreply.github.com>
2018-05-14 15:48:51 -0500

    Updated getWeather
    Changed the quantity of each piece of info to 4 and added days

--- getWeather.py
+++ getWeather.py
@@ -6,6 +6,7 @@
 #################################################################################
 import requests
 import sys
+from datetime import date
 
 # ---- IMPORTANT VALUES ---- #
 # rainData
@@ -23,6 +24,8 @@ def degreesToCardinal(deg):
     return dirs[ix % 16]
 
 def getData():
+    values = {}
+
     # ---- KEYS ---- #
     DSKey = open("DARKSKY_KEY.txt", "r").read().strip("\n")
     OWMKey = open("OPEN_WEATHER_KEY.txt", "r").read().strip("\n")
@@ -36,7 +39,7 @@ def getData():
     response = response.json()
     rainData = []
     rainData.append(response["currently"]["precipProbability"]*100)
-    for i in range(3):
+    for i in range(4):
         rainData.append(response["daily"]["data"][1+i]["precipProbability"]*100)
 
     response = requests.get("http://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&APPID={}".format(latitude, longitude, OWMKey))
@@ -47,6 +50,9 @@ def getData():
     windSpeed = windData["speed"]
     windDirection = degreesToCardinal(int(windData["deg"]))
 
+    values["windspeed"] = windSpeed
+    values["direction"] = windDirection
+
     # but we need more data for the temperatures, descriptions, and images
     tempMaxData = []
     tempMinData = []
@@ -56,7 +62,7 @@ def getData():
     # Grab all the temperature data.
     # The API returns information for every three hours over 5 days
     # Grabbing every eighth value makes it grab values for every 24 hours
-    for i in range(0, len(response["list"]), 8):
+    for i in range(0, 25, 8):
         tempMinData.append(response["list"][i]["main"]["temp_min"])
         tempMaxData.append(response["list"][i]["main"]["temp_max"])
         descriptionData.append(response["list"][i]["weather"][0]["description"])
@@ -71,4 +77,19 @@ def getData():
         tempMinData[i] = (int(tempMinData[i]) - 273) * 9 / 5 + 32
         tempMaxData[i] = (int(tempMaxData[i]) - 273) * 9 / 5 + 32 
 
-    return rainData, windSpeed, windDirection, tempMaxData, tempMinData, descriptionData, imageData
+    # put everything in a dictionary to be sent to the template
+    for i in range(len(tempMaxData)):
+        values["{}Precip".format(i)] = rainData[i]
+        values["{}High".format(i)] = tempMaxData[i]
+        values["{}Low".format(i)] = tempMinData[i]
+        values["{}day".format(i)] = descriptionData[i]
+        values["{}img".format(i)] = imageData[i]
+
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    for i in range(1, 4):
+        # this takes advantage of the fact that you can access elements from the back (negative indices)
+        # but you can't go over.
+        # the indices will always be negative but that's okay
+        values["{}dow".format(i)] = days[i - 7 - date.weekday(date.today())]
+
+    return values
